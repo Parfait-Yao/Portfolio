f22508169decb17099ee0cb947b1ea07d4f4f4ba
yao-dev-hub <parfaitericyao123@gmail.com>
2026-04-08 08:03:17 +0000

    3

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -160,7 +160,7 @@ export default async function Home() {
           </div>
 
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-10 mb-16 w-full text-left">
-            {featuredProjects.map((project) => (
+            {featuredProjects.map((project: any) => (
               <ProjectCard key={project.id} project={project} />
             ))}
           </div>
